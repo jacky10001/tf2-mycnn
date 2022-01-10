c34e419ed3603dd8233ca5a54e7abd440c08b44c
Jacky Gao <50270079+jacky10001@users.noreply.github.com>
2022-01-10 22:35:01 +0800

    加入VGG論文架構 (有使用BN、Dropout)
    四種模型架構，分別是 VGG11、VGG13、VGG16、VGG19
    
    加入VGG16範例
    
    :alarm_clock: TODO: 嘗試載入Keras預訓練權重

--- mycnn/__init__.py
+++ mycnn/__init__.py
@@ -6,6 +6,10 @@ from . import utils
 
 from .lenet import LeNet5
 from .alexnet import AlexNet
+from .vgg import VGG11
+from .vgg import VGG13
+from .vgg import VGG16
+from .vgg import VGG19
 
-__version__ = "v0.22.01.09"
+__version__ = "v0.22.01.10"
 __maintainer__ = "Jacky"
---
+++ mycnn/vgg.py
@@ -0,0 +1,392 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from .core.base_model import KerasModel
+
+
+class VGG11(KerasModel):  
+    def __init__(self,
+                 input_shape=(224, 224, 3),
+                 classes_num=1000,
+                 **kwargs):
+        """ VGG11 (Type-A)
+        Note: 有加入 BatchNormalization Layer
+        """
+        self.input_shape = input_shape
+        self.classes_num = classes_num
+        super().__init__(**kwargs)
+      
+    def build(self):
+        x_in = layers.Input(shape=self.input_shape, name="image")
+
+        # Common parameter for layer
+        padding = 'same'
+        kernel_size = (3, 3)
+        pool_size = (2, 2)
+
+        # block 1
+        block_name = "block1"
+        filters = 64
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x_in)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 2
+        block_name = "block2"
+        filters = 128
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 3
+        block_name = "block3"
+        filters = 256
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 4
+        block_name = "block4"
+        filters = 512
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 5
+        block_name = "block5"
+        filters = 512
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        x = layers.Flatten(name="flatten")(x)
+
+        block_name = "fc1"
+        x = layers.Dense(4096, name=block_name)(x)
+        x = layers.ReLU(name=block_name+"_relu")(x)
+        x = layers.Dropout(0.5, name=block_name+"_dropout")(x)
+
+        block_name = "fc2"
+        x = layers.Dense(4096, name=block_name)(x)
+        x = layers.ReLU(name=block_name+"_relu")(x)
+        x = layers.Dropout(0.5, name=block_name+"_dropout")(x)
+
+        x_out = layers.Dense(self.classes_num, activation='softmax', name="predictions")(x)
+        
+        self.setup_model(x_in, x_out, name="VGG11")
+
+
+class VGG13(KerasModel):  
+    def __init__(self,
+                 input_shape=(224, 224, 3),
+                 classes_num=1000,
+                 **kwargs):
+        """ VGG11 (Type-B)
+        Note: 有加入 BatchNormalization Layer
+        """
+        self.input_shape = input_shape
+        self.classes_num = classes_num
+        super().__init__(**kwargs)
+      
+    def build(self):
+        x_in = layers.Input(shape=self.input_shape, name="image")
+
+        # Common parameter for layer
+        padding = 'same'
+        kernel_size = (3, 3)
+        pool_size = (2, 2)
+
+        # block 1
+        block_name = "block1"
+        filters = 64
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x_in)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 2
+        block_name = "block2"
+        filters = 128
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 3
+        block_name = "block3"
+        filters = 256
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 4
+        block_name = "block4"
+        filters = 512
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 5
+        block_name = "block5"
+        filters = 512
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        x = layers.Flatten(name="flatten")(x)
+
+        block_name = "fc1"
+        x = layers.Dense(4096, name=block_name)(x)
+        x = layers.ReLU(name=block_name+"_relu")(x)
+        x = layers.Dropout(0.5, name=block_name+"_dropout")(x)
+
+        block_name = "fc2"
+        x = layers.Dense(4096, name=block_name)(x)
+        x = layers.ReLU(name=block_name+"_relu")(x)
+        x = layers.Dropout(0.5, name=block_name+"_dropout")(x)
+
+        x_out = layers.Dense(self.classes_num, activation='softmax', name="predictions")(x)
+        
+        self.setup_model(x_in, x_out, name="VGG13")
+
+
+class VGG16(KerasModel):  
+    def __init__(self,
+                 input_shape=(224, 224, 3),
+                 classes_num=1000,
+                 **kwargs):
+        """ VGG16 (Type-D)
+        Note: 有加入 BatchNormalization Layer
+        """
+        self.input_shape = input_shape
+        self.classes_num = classes_num
+        super().__init__(**kwargs)
+      
+    def build(self):
+        x_in = layers.Input(shape=self.input_shape, name="image")
+
+        # Common parameter for layer
+        padding = 'same'
+        kernel_size = (3, 3)
+        pool_size = (2, 2)
+
+        # block 1
+        block_name = "block1"
+        filters = 64
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x_in)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 2
+        block_name = "block2"
+        filters = 128
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 3
+        block_name = "block3"
+        filters = 256
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv3")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn3")(x)
+        x = layers.ReLU(name=block_name+"_relu3")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 4
+        block_name = "block4"
+        filters = 512
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv3")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn3")(x)
+        x = layers.ReLU(name=block_name+"_relu3")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 5
+        block_name = "block5"
+        filters = 512
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv3")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn3")(x)
+        x = layers.ReLU(name=block_name+"_relu3")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        x = layers.Flatten(name="flatten")(x)
+
+        block_name = "fc1"
+        x = layers.Dense(4096, name=block_name)(x)
+        x = layers.ReLU(name=block_name+"_relu")(x)
+        x = layers.Dropout(0.5, name=block_name+"_dropout")(x)
+
+        block_name = "fc2"
+        x = layers.Dense(4096, name=block_name)(x)
+        x = layers.ReLU(name=block_name+"_relu")(x)
+        x = layers.Dropout(0.5, name=block_name+"_dropout")(x)
+
+        x_out = layers.Dense(self.classes_num, activation='softmax', name="predictions")(x)
+        
+        self.setup_model(x_in, x_out, name="VGG16")
+
+
+class VGG19(KerasModel):  
+    def __init__(self,
+                 input_shape=(224, 224, 3),
+                 classes_num=1000,
+                 **kwargs):
+        """ VGG19 (Type-E)
+        Note: 有加入 BatchNormalization Layer
+        """
+        self.input_shape = input_shape
+        self.classes_num = classes_num
+        super().__init__(**kwargs)
+      
+    def build(self):
+        x_in = layers.Input(shape=self.input_shape, name="image")
+
+        # Common parameter for layer
+        padding = 'same'
+        kernel_size = (3, 3)
+        pool_size = (2, 2)
+
+        # block 1
+        block_name = "block1"
+        filters = 64
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x_in)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 2
+        block_name = "block2"
+        filters = 128
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 3
+        block_name = "block3"
+        filters = 256
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv3")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn3")(x)
+        x = layers.ReLU(name=block_name+"_relu3")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv4")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn4")(x)
+        x = layers.ReLU(name=block_name+"_relu4")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 4
+        block_name = "block4"
+        filters = 512
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv3")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn3")(x)
+        x = layers.ReLU(name=block_name+"_relu3")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv4")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn4")(x)
+        x = layers.ReLU(name=block_name+"_relu4")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        # block 5
+        block_name = "block5"
+        filters = 512
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv1")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn1")(x)
+        x = layers.ReLU(name=block_name+"_relu1")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv2")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn2")(x)
+        x = layers.ReLU(name=block_name+"_relu2")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv3")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn3")(x)
+        x = layers.ReLU(name=block_name+"_relu3")(x)
+        x = layers.Conv2D(filters, kernel_size, padding=padding, name=block_name+"_conv4")(x)
+        x = layers.BatchNormalization(name=block_name+"_bn4")(x)
+        x = layers.ReLU(name=block_name+"_relu4")(x)
+        x = layers.MaxPooling2D(pool_size=pool_size, name=block_name+"_pool")(x)
+
+        x = layers.Flatten(name="flatten")(x)
+
+        block_name = "fc1"
+        x = layers.Dense(4096, name=block_name)(x)
+        x = layers.ReLU(name=block_name+"_relu")(x)
+        x = layers.Dropout(0.5, name=block_name+"_dropout")(x)
+
+        block_name = "fc2"
+        x = layers.Dense(4096, name=block_name)(x)
+        x = layers.ReLU(name=block_name+"_relu")(x)
+        x = layers.Dropout(0.5, name=block_name+"_dropout")(x)
+
+        x_out = layers.Dense(self.classes_num, activation='softmax', name="predictions")(x)
+        
+        self.setup_model(x_in, x_out, name="VGG19")
