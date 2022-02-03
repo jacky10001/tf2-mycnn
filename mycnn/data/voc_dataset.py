c8e8019821918fc8fec6a856299e8769f34a582e
Jacky Gao <50270079+jacky10001@users.noreply.github.com>
2022-02-03 17:24:41 +0800

    FCN 範例程式 (#37)
    * 建立 FCN 範例程式
    
    * 自動下載 VOC 2012 Dataset

---
+++ mycnn/data/voc_dataset.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+import tarfile
+import requests
+import tensorflow as tf
+import cv2
+
+
+def download_pascal_voc_dataset(dataset_path):
+    # 自動設定相關路徑
+    main_folder = os.path.join(dataset_path, "VOC")
+    voc2012_folder = os.path.join(main_folder, "voc2012")
+    tar_file = os.path.join(main_folder, "VOCtrainval_11-May-2012.tar")
+    flag = True
+
+    if not os.path.exists(main_folder):
+        os.makedirs(main_folder, exist_ok=True)
+
+    # 從 MS Center 下載資料集
+    if not os.path.exists(tar_file):
+        url = "http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar"
+        r = requests.get(url, allow_redirects=True)
+        open(tar_file, "wb").write(r.content)
+    else:
+        print("Already download tar file.")
+    
+    if not os.path.exists(voc2012_folder):
+        # 解壓縮檔案至暫存資料夾
+        with tarfile.TarFile(tar_file, 'r') as tar_ref:
+            print(f"Untarring {tar_file} ...")
+            os.makedirs(voc2012_folder, exist_ok=True)
+            tar_ref.extractall(voc2012_folder)
+    else:
+        print("Already Uutar tar file.")
