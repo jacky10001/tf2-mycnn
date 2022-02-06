30ffc3e901907cb189949e092c9b271317285433
Jacky Gao <50270079+jacky10001@users.noreply.github.com>
2022-02-06 17:10:33 +0800

    Restore unet mode

--- mycnn/data/segmentation.py
+++ mycnn/data/segmentation.py
@@ -171,26 +171,17 @@ def generate_segmentation_dataset(directory,
         y = tf.io.read_file(y)
         y = tf.io.decode_image(y, channels=1, expand_animations=False)
         y = tf.image.resize(y, mask_size, method="nearest")  # 使用近鄰插植，處理物體邊緣值維持一致
+        if mask_mode == 1:
+            h_diff = image_size[0] - mask_size[0]
+            w_diff = image_size[1] - mask_size[0]
+            h_start = tf.cast(h_diff/2, tf.int32)
+            w_start = tf.cast(w_diff/2, tf.int32)
+            y = tf.image.crop_to_bounding_box(y, h_start, w_start, mask_size[0], mask_size[1])
         y = tf.cast(y, tf.uint8)
         y = tf.reshape(y, (-1,))
         y = tf.one_hot(y, classes_num)
         # y = tf.reshape(y, mask_size+(classes_num,))
         return x, y
-
-    def load_ori_unet_mask(y):
-        y = tf.io.read_file(y)
-        y = tf.io.decode_image(y, channels=1, expand_animations=False)
-        y = tf.image.resize(y, image_size, method="nearest")  # 使用近鄰插植，處理物體邊緣值維持一致
-        h_diff = image_size[0] - mask_size[0]
-        w_diff = image_size[1] - mask_size[0]
-        h_start = tf.cast(h_diff/2, tf.int32)
-        w_start = tf.cast(w_diff/2, tf.int32)
-        y = tf.image.crop_to_bounding_box(y, h_start, w_start, mask_size[0], mask_size[1])
-        y = tf.cast(y, tf.uint8)
-        y = tf.reshape(y, (-1,))
-        y = tf.one_hot(y, classes_num)
-        # y = tf.reshape(y, mask_size+(classes_num,))
-        return y
     
     if not validation_split:
         img_path_ds = tf.data.Dataset.from_tensor_slices(image_paths)
